1806e8ab2f80a8638acfd2049164963af27baf23
Ashwinitest19 <ashwini.more@nividous.com>
2021-12-28 15:15:52 +0530

    This is third commit!

--- src/testCases/TC_001_Login.java
+++ src/testCases/TC_001_Login.java
@@ -8,14 +8,18 @@ import utility.Util;
 public class TC_001_Login extends Util
 
 { 
+	
 	@Test
-	public void loginHRM() throws Exception
+	public void loginHRM()throws Exception
 	{
 		launchBrowser();
-		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("admin");
-		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("admin123");
-		driver.findElement(By.xpath("//input[@id='btnLogin']")).click();
+		Config();
+		driver.findElement(By.xpath(getuserName("Login_UserName"))).sendKeys("admin");
+		driver.findElement(By.xpath(getpassword("Login_Password"))).sendKeys("Admin123");
+		driver.findElement(By.xpath(clickbutton("Button"))).click();
+		
 		closeBrowser();
 	}
+	
 	
 }
--- src/utility/Util.java
+++ src/utility/Util.java
@@ -1,12 +1,16 @@
 package utility;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Util
 {
   public  WebDriver driver;
-  
+  Properties po;
   public void launchBrowser()
   {
 	  System.setProperty("webdriver.chrome.driver","C:\\Users\\Ashwini.More\\Downloads\\chromedriver.exe");
@@ -14,6 +18,43 @@ public class Util
 	  driver.get("https://opensource-demo.orangehrmlive.com/");
 	  driver.manage().window().maximize();
   }
+  
+  public void Config()
+  {
+	  
+	  //File src = new File ("C:\\Users\\Ashwini.More\\workspace\\Practice\\LoginHRM\\TestData\\config.properties");
+	  
+	  try {
+		  File src = new File ("C:\\Users\\Ashwini.More\\workspace\\Practice\\LoginHRM\\TestData\\config.properties");
+	  FileInputStream fi = new FileInputStream(src);
+	  po = new Properties();
+	  po.load(fi);
+	  
+	  }
+	  catch (Exception e)
+	  {
+		  System.out.println(e);
+	  }
+  }
+  public String getuserName(String Login_UserName)
+  {
+	 String value = po.getProperty(Login_UserName);
+	return value;
+	
+	  
+  }
+  public String getpassword(String Login_Password)
+  {
+	 String value = po.getProperty(Login_Password);
+	return value;
+	  
+  }
+  public String clickbutton(String Button)
+  {
+	  String value = po.getProperty(Button);
+  return value;
+  }
+  
   
   public void closeBrowser() throws Exception
   {
